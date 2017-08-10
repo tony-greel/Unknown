6a9197a69ae6b58cddf84868183aa4639d3aeb20
Administrator <1123600675@qq.com>
2017-08-10 14:02:05 +0800

    聊天

--- app/src/main/java/com/ljj/unknown/activity/fragment/PersonalFragment.java
+++ app/src/main/java/com/ljj/unknown/activity/fragment/PersonalFragment.java
@@ -2,15 +2,11 @@ package com.ljj.unknown.activity.fragment;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ljj.unknown.R;
-
-import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/8/4.
